101b382ea2db5065425da87f38b4203ecdc93404
lindgrenleonard <lindgrenleonard@gmail.com>
2022-11-02 16:00:13 +0100

    adding quick conversion for parsed time to unix time, still broken

--- test.c
+++ test.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
-char* stationParse(char *buff) {
+char *stationParse(char *buff)
+{
 
     char *time;
     char *firstCutoff;
@@ -12,30 +13,47 @@ char* stationParse(char *buff) {
 
     time = strtok(secondCutoff, ",");
     time = strstr(time, "20");
-    //printf("%s", time);
-    
-    return(time);
+    // printf("%s", time);
+
+    return (time);
 }
 
-
-
-char* fileRead() {
+char *fileRead()
+{
     FILE *fp;
     char buff[10000];
-    
+
     fp = fopen("jsondata2.json", "r");
     fgets(buff, 9999, fp);
-    return(stationParse(buff));
+    return (stationParse(buff));
 }
 
+int toEpochTime(char *charTime)
+{
+    int time;
+    char *year = strtok(charTime, "-");
+    char *month = strtok(NULL, "-");
+    char *day = strtok(NULL, "T");
+    char *hour = strtok(NULL, ":");
+    char *min = strtok(NULL, ":");
+    char *sec = strtok(NULL, "\"");
+    int y, m, d, h, mi, s;
+    sscanf(year, "%d", &y);
+    sscanf(month, "%d", &m);
+    sscanf(day, "%d", &d);
+    sscanf(hour, "%d", &h);
+    sscanf(min, "%d", &mi);
+    sscanf(sec, "%d", &s);
+    time = (((y - 1970) * 365.25) * 86400 + (m * 2629743) + (d * 86400) + (h * 3600) + (m * 60) + s);
+    return time;
+}
 
+int main()
+{
 
-
-
-int main() {
-
-    printf("Hello World!\n");
+    printf("Hello World!\n\n");
     puts(fileRead());
-    return(0);
+    int unixTime = toEpochTime(fileRead());
+    printf("%d", unixTime);
+    return (0);
 }
-
