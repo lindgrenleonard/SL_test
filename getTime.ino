caf59f4ade83fd8aaabc6189c04bc5742f533b58
Leonard Lindgren <65599046+lindgrenleonard@users.noreply.github.com>
2022-12-08 12:24:21 +0100

    Update getTime.ino
    added compare function and rewrote syntax to use strings

--- getTime.ino
+++ getTime.ino
@@ -1,61 +1,116 @@
+#include <NTPClient.h>
 #include <ESP8266WiFi.h>
-#include <time.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiUdp.h>
 
 const char *ssid     = "Airhome";
 const char *password = "Coco2020";
 
-const char* NTP_SERVER = "ch.pool.ntp.org";
-const char* TZ_INFO = "CET-1CEST,M3.5.0,M10.5.0/3";
 
-tm timeinfo;
-time_t now;
-long unsigned lastNTPtime;
-unsigned long lastEntryTime;
+HTTPClient http;
+const char* serverName = "http://api.sl.se/api2/realtimedeparturesV4.jsonl?key=938f37c5783949a68179014a1315976d&siteid=9201&timewindow=30&Metro=false";
 
 
-void setup() {
+// Define NTP Client to get time
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", 3600);
+
+
+//Parses out the time in from the json and returns it as number of seconds until bus arrival
+int toTimes(char *charTime) {
+    int buff, time = 0;
+    char *hour = strtok(NULL, ":");
+    char *min = strtok(NULL, ":");
+    char *sec = strtok(NULL, "\"");
+    sscanf(hour, "%d", &buff);
+    time += buff*3600;
+    sscanf(min, "%d", &buff);
+    time += buff*60;
+    sscanf(sec, "%d", &buff);
+    time += buff;
+    
+    return(time);
+}
+
+//Finds the expected time of bus arrival as a char pointer, parsed from big json file
+int arrivalTimeParse(String jsonFile) {
+    char *time;
+    int index = jsonFile.indexOf("Jakobsbergs station");
+
+    if(index == -1) {
+      Serial.println("Could not find");
+      return -1;
+    }
+
+    String firstCutoff = jsonFile.substring(index);
+    index = firstCutoff.indexOf("ExpectedDateTime");
+    String secondCutoff = firstCutoff.substring(index);
+    char buff[secondCutoff.length()+1];
+    secondCutoff.toCharArray(buff, 1000);
+    time = strtok(buff, ",");
+    time = strstr(time, "20");
+    int arrivalTime = toTimes(time);
+    return arrivalTime;
+}
+
+//Reads the json file we want to parse
+
+
+//Finds the urgency level of the arrival. Less time until the bus means higher urgency, ranked 0-3
+int getUrgency(int hours, int minutes, int seconds, String jsonFile) {
+    int urgency;
+    int busTime = arrivalTimeParse(jsonFile);
+    if(busTime = -1) return;
+    int currTime = (hours*3600 + minutes*60 + seconds);
+    int timeDiff = busTime-currTime;
+    
+    if(timeDiff < 6*60 && timeDiff > 3*60) {urgency = 1;}
+    if(timeDiff < 10*60 && timeDiff > 6*60) {urgency = 2;}
+    if(timeDiff > 10*60) {urgency = 3;}
+    else {urgency = 0;}
+    return urgency;
+}
+
+
+void setup(){
   Serial.begin(115200);
-  Serial.println("\n\nNTP Time Test\n");
+
   WiFi.begin(ssid, password);
 
   while ( WiFi.status() != WL_CONNECTED ) {
     delay ( 500 );
     Serial.print ( "." );
   }
-  Serial.println("\n\nWiFi connected\n\n");
-
-  configTime(0, 0, NTP_SERVER);
-  setenv("TZ", TZ_INFO, 1);
-
-  if (getNTPtime(10)) {  // wait up to 10sec to sync
-  } 
-  
-  else {
-    Serial.println("Time not set");
-    ESP.restart();
-  }
-
-  lastNTPtime = time(&now);
-
+  Serial.println("Wifi Connected");
+  timeClient.begin();
+  WiFiClient client;
+  http.begin(client, serverName);
 }
 
 void loop() {
-  Serial.print(getNTPtime(10)+3600);
-  Serial.print("\n");
-  delay(1000);
-}
-
-
-int getNTPtime(int sec) {
-
-  {
-    uint32_t start = millis();
-    do {
-      time(&now);
-      localtime_r(&now, &timeinfo);
-      delay(10);
-    } while (((millis() - start) <= (1000 * sec)) && (timeinfo.tm_year < (2016 - 1900)));
-    if (timeinfo.tm_year <= (2016 - 1900)) return false;  // the NTP call was not successful
+  int urgency;
+  timeClient.update();
+  Serial.println(timeClient.getHours());
+  Serial.println(timeClient.getMinutes());
+  Serial.println(timeClient.getSeconds());
+  
+  
+  int httpCode = http.GET();
+  String jsonFile;
+  // Check the jsonFile code
+  if (httpCode == HTTP_CODE_OK) {
+    // Read the jsonFile from the server
+    jsonFile += http.getString();
+    Serial.println("Http jsonFile: " + jsonFile);
+  } else {
+    // Handle error cases
+    Serial.println("Error: " + String(httpCode));
   }
-  return now;
+
+  urgency = getUrgency(timeClient.getHours(), timeClient.getMinutes(), timeClient.getSeconds(), jsonFile);
+  Serial.println("Urgency: " + urgency);
+  //pinMode(LED_PIN, OUTPUT);
+  //analogWrite(LED_PIN, 1);
+
+  delay(100000);
 }
