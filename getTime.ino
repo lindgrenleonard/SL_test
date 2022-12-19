6826f18893249d3d5dd2662f0c6b2fa2fef2855d
Leonard Lindgren <65599046+lindgrenleonard@users.noreply.github.com>
2022-12-19 11:02:15 +0100

    Add files via upload

--- getTime.ino
+++ getTime.ino
@@ -1,20 +1,20 @@
 #include <NTPClient.h>
 #include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
+//#include <ESP8266HTTPClient.h>
+#include <ArduinoHttpClient.h>
 #include <WiFiUdp.h>
 
 const char *ssid     = "Airhome";
 const char *password = "Coco2020";
 
-
-HTTPClient http;
-const char* serverName = "http://api.sl.se/api2/realtimedeparturesV4.jsonl?key=938f37c5783949a68179014a1315976d&siteid=9201&timewindow=30&Metro=false";
-
+const char kHostname[] = "https://api.sl.se";
+const char kPath[] = "/api2/realtimedeparturesV4.xml?key=938f37c5783949a68179014a1315976d&siteid=9201&timewindow=30&Metro=false";
+WiFiClient c;
+HttpClient http(c, kHostname);
 
 // Define NTP Client to get time
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org", 3600);
-
 
 //Parses out the time in from the json and returns it as number of seconds until bus arrival
 int toTimes(char *charTime) {
@@ -53,14 +53,11 @@ int arrivalTimeParse(String jsonFile) {
     return arrivalTime;
 }
 
-//Reads the json file we want to parse
-
-
 //Finds the urgency level of the arrival. Less time until the bus means higher urgency, ranked 0-3
 int getUrgency(int hours, int minutes, int seconds, String jsonFile) {
     int urgency;
     int busTime = arrivalTimeParse(jsonFile);
-    if(busTime = -1) return;
+    if(busTime == -1) return -1;
     int currTime = (hours*3600 + minutes*60 + seconds);
     int timeDiff = busTime-currTime;
     
@@ -70,7 +67,6 @@ int getUrgency(int hours, int minutes, int seconds, String jsonFile) {
     else {urgency = 0;}
     return urgency;
 }
-
 
 void setup(){
   Serial.begin(115200);
@@ -83,34 +79,47 @@ void setup(){
   }
   Serial.println("Wifi Connected");
   timeClient.begin();
-  WiFiClient client;
-  http.begin(client, serverName);
 }
 
 void loop() {
-  int urgency;
-  timeClient.update();
-  Serial.println(timeClient.getHours());
-  Serial.println(timeClient.getMinutes());
-  Serial.println(timeClient.getSeconds());
-  
-  
-  int httpCode = http.GET();
   String jsonFile;
-  // Check the jsonFile code
-  if (httpCode == HTTP_CODE_OK) {
-    // Read the jsonFile from the server
-    jsonFile += http.getString();
-    Serial.println("Http jsonFile: " + jsonFile);
-  } else {
-    // Handle error cases
-    Serial.println("Error: " + String(httpCode));
+  int err =0;
+
+  timeClient.update();
+  int bodyLen = http.contentLength();
+
+
+    err = http.get(kPath);
+  if (err == 0)
+  {
+      Serial.println("startedRequest ok");
+
+      err = http.responseStatusCode();
+      if (err >= 0)
+      {
+        Serial.print("Got status code: ");
+        Serial.println(err);
+
+        int bodyLen = http.contentLength();
+        Serial.print("Content length is: ");
+        Serial.println(bodyLen);
+
+        char c;
+      // Whilst we haven't timed out & haven't reached the end of the body
+      while ( (http.connected() || http.available()) &&
+             (!http.endOfBodyReached()))
+      {
+          if (http.available())
+          {
+              c = http.read();
+              // Print out this character
+              Serial.print(c);
+          }
+    }
   }
-
-  urgency = getUrgency(timeClient.getHours(), timeClient.getMinutes(), timeClient.getSeconds(), jsonFile);
-  Serial.println("Urgency: " + urgency);
-  //pinMode(LED_PIN, OUTPUT);
-  //analogWrite(LED_PIN, 1);
-
-  delay(100000);
+  //urgency = getUrgency(timeClient.getHours(), timeClient.getMinutes(), timeClient.getSeconds(), jsonFile);
+  //Serial.println("Urgency: " + urgency);
+    
+  }
+while(1); 
 }
