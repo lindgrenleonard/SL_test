04d689c61b20547c411a74611f7a01a379139a00
lindgrenleonard <lindgrenleonard@gmail.com>
2023-03-31 00:17:06 +0200

    test

--- test.c
+++ test.c
@@ -1,41 +1,68 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
-char* stationParse(char *buff) {
+char *stationParse(char *buff)
+{
 
     char *time;
     char *firstCutoff;
     char *secondCutoff;
 
     firstCutoff = strstr(buff, "Jakobsbergs station");
-    secondCutoff = strstr(firstCutoff, "ExpectedDateTime");
+    secondCutoff = strstr(buff, "ExpectedDateTime");
 
     time = strtok(secondCutoff, ",");
     time = strstr(time, "20");
-    //printf("%s", time);
-    
-    return(time);
+    printf("%s", time);
+
+    struct tm date_time = {0};
+    if (strptime(time, &date_time) == NULL)
+    {
+        // Handle error
+    }
+
+    // Convert the struct tm object into a Unix time
+    time_t unix_time = mktime(&date_time);
+    printf("Unix time: %ld\n", unix_time);
+    return (time);
 }
 
-
-
-char* fileRead() {
+char *fileRead()
+{
     FILE *fp;
     char buff[10000];
-    
-    fp = fopen("jsondata2.json", "r");
+
+    fp = fopen("jsondata3.json", "r");
     fgets(buff, 9999, fp);
-    return(stationParse(buff));
+    return (stationParse(buff));
 }
 
+int toTimes(char *charTime)
+{
+    int buff, time = 0;
+    char *year = strtok(charTime, "-");
+    char *month = strtok(NULL, "-");
+    char *day = strtok(NULL, "T");
+    char *hour = strtok(NULL, ":");
+    char *min = strtok(NULL, ":");
+    char *sec = strtok(NULL, "\"");
+    sscanf(hour, "%d", &buff);
+    time += buff * 3600;
+    sscanf(min, "%d", &buff);
+    time += buff * 60;
+    sscanf(sec, "%d", &buff);
+    time += buff;
 
+    return (time);
+}
 
-
-
-int main() {
-
+int main()
+{
+    int dates;
     printf("Hello World!\n");
-    puts(fileRead());
-    return(0);
-}
+    dates = toTimes(fileRead());
 
+    return (0);
+}
